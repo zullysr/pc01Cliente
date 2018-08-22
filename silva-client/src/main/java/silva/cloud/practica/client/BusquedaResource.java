c9eccb53d81f637c31a188330ff8cace2f505043
zullysr <42480348+zullysr@users.noreply.github.com>
2018-08-22 17:20:04 -0500

    Update BusquedaResource.java

--- silva-client/src/main/java/silva/cloud/practica/client/BusquedaResource.java
+++ silva-client/src/main/java/silva/cloud/practica/client/BusquedaResource.java
@@ -5,22 +5,21 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/rest/buscar/client")
+@RequestMapping("/rest/buscar/client/{id}")
 public class BusquedaResource {
 
 	@Autowired
 	private RestTemplate restTemplate;
 	
 	@GetMapping
-	@RequestMapping("/{id}")
-	public String buscar(@RequestParam Integer id) {
-		String url ="http://localhost:8071/rest/buscar/server/{id}";
+	public String buscar(@PathVariable Integer id) {
+		String url ="http://localhost:8071/rest/buscar/server/busca/{id}";
 		Map<String, Object> parameters = new HashMap<String, Object>();
 		parameters.put("id", id);
 		return restTemplate.getForObject(url, String.class, parameters);
